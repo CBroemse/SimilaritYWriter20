3f662064acb26fc5b841a23f3aa6ca7ad1a857c0
Cee <35766903+CBroemse@users.noreply.github.com>
2020-07-14 21:23:00 +0100

    Update mySketch.js

--- src/p5SimY/mySketch.js
+++ src/p5SimY/mySketch.js
@@ -14,7 +14,7 @@ function setup() {
 	
 	rev = 1
 	
-	equation = "0.6711409395973155*x =1.05062082139446*y+10.52123552123552*z"
+	equation = "0.6711409395973155*x*y*z=1.05062082139446*y+10.52123552123552*z+x"
 	addButtons()
 	equation = cleanEquation(equation)
 	
