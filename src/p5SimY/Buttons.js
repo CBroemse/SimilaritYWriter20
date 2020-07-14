2dc2387c9dd939063f591fbc0745f369f4f35be7
Cee <35766903+CBroemse@users.noreply.github.com>
2020-07-14 20:49:31 +0100

    Update Buttons.js

--- src/p5SimY/Buttons.js
+++ src/p5SimY/Buttons.js
@@ -17,11 +17,12 @@ function addButtons() {
 	labelYw = createDiv("Y-Scale")
 	labelYw.position(30, 150)
 	inputYw = createInput();
-	inputYw.size(60, 320)
+	inputYw.size(60, 20)
 	inputYw.value(0.1)
 	inputYw.position(30, 175);
 	inputYw.input(function() {
 		yw = inputYw.value()
+		labelYw.position(30, 350)
 	})
 
 
